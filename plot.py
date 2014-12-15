f7b11b32fab3f9875944c03da25f356275c7585d
Nadja Deininger <nadja@ef.gy>
2014-12-15 13:08:39 +0300

    plot centroids in blue

--- plot.py
+++ plot.py
@@ -6,5 +6,5 @@ import matplotlib.pyplot as plt
 dataset = [(random.randint(0, 1000), random.randint(0, 1000)) for _ in range(200)]
 k = 5
 clustering, centroids = kmeans.lloyds_algorithm(dataset, k)
-plt.plot([x[0] for x in dataset], [x[1] for x in dataset], 'ro')
+plt.plot([x[0] for x in dataset], [x[1] for x in dataset], 'ro', [c[0] for c in centroids], [c[1] for c in centroids], 'bo')
 plt.show()
